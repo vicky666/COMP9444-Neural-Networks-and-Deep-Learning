235a25134c904437b821cdc4e7477f466ee42977
oreo <apple@pawpaws-MacBook-Air.local>
2020-04-04 12:46:04 +1100

    First Commit

---
+++ hw1/code/part3.py
@@ -0,0 +1,254 @@
+#!/usr/bin/env python3
+"""
+part3.py
+
+UNSW COMP9444 Neural Networks and Deep Learning
+
+ONLY COMPLETE METHODS AND CLASSES MARKED "TODO".
+
+DO NOT MODIFY IMPORTS. DO NOT ADD EXTRA FUNCTIONS.
+DO NOT MODIFY EXISTING FUNCTION SIGNATURES.
+DO NOT IMPORT ADDITIONAL LIBRARIES.
+DOING SO MAY CAUSE YOUR CODE TO FAIL AUTOMATED TESTING.
+"""
+import torch
+from torchvision import datasets, transforms
+from torch import nn, optim
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class Linear(nn.Module):
+    """
+    DO NOT MODIFY
+    Linear (10) -> ReLU -> LogSoftmax
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(784, 10)
+
+    def forward(self, x):
+        x = x.view(x.shape[0], -1)  # make sure inputs are flattened
+
+        x = F.relu(self.fc1(x))
+        x = F.log_softmax(x, dim=1)  # preserve batch dim
+
+        return x
+
+
+class FeedForward(nn.Module):
+    """
+    TODO: Implement the following network structure
+    Linear (256) -> ReLU -> Linear(64) -> ReLU -> Linear(10) -> ReLU-> LogSoftmax
+    """
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(784, 256)
+        self.fc2 = nn.Linear(256, 64)
+        self.fc3 = nn.Linear(64, 10)
+        
+    def forward(self, x):
+        x1 = x.view(x.shape[0], -1)
+        
+        x2 = F.relu(self.fc1(x1))
+        x3 = F.relu(self.fc2(x2))
+        x4 = F.relu(self.fc3(x3))
+        
+        x5 = F.log_softmax(x4, dim=1)
+        
+        return x5
+        
+
+
+class CNN(nn.Module):
+    """
+    TODO: Implement CNN Network structure
+
+    conv1 (channels = 10, kernel size= 5, stride = 1) -> Relu -> max pool (kernel size = 2x2) ->
+    conv2 (channels = 50, kernel size= 5, stride = 1) -> Relu -> max pool (kernel size = 2x2) ->
+    Linear (256) -> Relu -> Linear (10) -> LogSoftmax
+
+
+    Hint: You will need to reshape outputs from the last conv layer prior to feeding them into
+    the linear layers.
+    """
+    #torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride=1)
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=10, kernel_size=5, stride=1)
+        self.conv2 = nn.Conv2d(in_channels=10, out_channels=50, kernel_size=5, stride=1)
+        
+        self.max_pool = nn.MaxPool2d(kernel_size=2)
+        
+        self.fc1 = nn.Linear(50*4*4, 256)
+        self.fc2 = nn.Linear(256, 10)
+        
+    def forward(self, x):
+        #(1,28,28)
+        x = F.relu(self.conv1(x))#(10,(28-5+1)=24,24)
+        x1 = self.max_pool(x)#(10,12,12)
+        x2 = F.relu(self.conv2(x1))#(50,12-5+1=8,8)
+        x3 = self.max_pool(x2)#(50,4,4)
+        
+        x4 = x3.view(-1, 50 * 4 * 4)
+        
+        x5 = F.relu(self.fc1(x4))
+        x6 = self.fc2(x5)
+        
+        x7 = F.log_softmax(x6, dim=1)
+        
+        return x7
+
+class NNModel:
+    def __init__(self, network, learning_rate):
+        """
+        Load Data, initialize a given network structure and set learning rate
+        DO NOT MODIFY
+        """
+
+        # Define a transform to normalize the data
+        transform = transforms.Compose([transforms.ToTensor(),
+                                        transforms.Normalize((0.5,), (0.5,))])
+
+        # Download and load the training data
+        trainset = datasets.KMNIST(root='./data', train=True, download=True, transform=transform)
+        self.trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=False)
+
+        # Download and load the test data
+        testset = datasets.KMNIST(root='./data', train=False, download=True, transform=transform)
+        self.testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False)
+
+        self.model = network
+
+        """
+        TODO: Set appropriate loss function such that learning is equivalent to minimizing the
+        cross entropy loss. Note that we are outputting log-softmax values from our networks,
+        not raw softmax values, so just using torch.nn.CrossEntropyLoss is incorrect.
+        
+        Hint: All networks output log-softmax values (i.e. log probabilities or.. likelihoods.). 
+        """
+        self.lossfn = nn.NLLLoss()
+        self.optimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
+
+        self.num_train_samples = len(self.trainloader)
+        self.num_test_samples = len(self.testloader)
+
+    def view_batch(self):
+        """
+        TODO: Display first batch of images from trainloader in 8x8 grid
+
+        Do not make calls to plt.imshow() here
+
+        Return:
+           1) A float32 numpy array (of dim [28*8, 28*8]), containing a tiling of the batch images,
+           place the first 8 images on the first row, the second 8 on the second row, and so on
+
+           2) An int 8x8 numpy array of labels corresponding to this tiling
+        """
+
+        dataiter = iter(self.trainloader)
+        images, labels = dataiter.next()
+        #print(images.shape)
+        #torch.Size([64, 1, 28, 28])
+        img1 = images.view(64,28,28)
+        #print(img1.shape)
+        #torch.Size([64, 28, 28])
+        img2 =img1.view(8,8,28,28)
+        #print(img2.shape)
+        img3 = img2.permute(0,2,1,3)
+        #print(img3.shape)
+        img4 = img3.reshape([8*28,8*28])
+        labels = labels.view(8, 8)
+        return img4,labels
+
+        
+
+    def train_step(self):
+        """
+        Used for submission tests and may be usefull for debugging
+        DO NOT MODIFY
+        """
+        self.model.train()
+        for images, labels in self.trainloader:
+            log_ps = self.model(images)
+            loss = self.lossfn(log_ps, labels)
+
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+            return
+
+    def train_epoch(self):
+        self.model.train()
+        for images, labels in self.trainloader:
+            log_ps = self.model(images)
+            loss = self.lossfn(log_ps, labels)
+
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+        return
+
+    def eval(self):
+        self.model.eval()
+        accuracy = 0
+        with torch.no_grad():
+            for images, labels in self.testloader:
+                log_ps = self.model(images)
+                ps = torch.exp(log_ps)
+                top_p, top_class = ps.topk(1, dim=1)
+                equals = top_class == labels.view(*top_class.shape)
+                accuracy += torch.mean(equals.type(torch.FloatTensor))
+
+        return accuracy / self.num_test_samples
+
+
+def plot_result(results, names):
+    """
+    Take a 2D list/array, where row is accuracy at each epoch of training for given model, and
+    names of each model, and display training curves
+    """
+    for i, r in enumerate(results):
+        plt.plot(range(len(r)), r, label=names[i])
+    plt.legend()
+    plt.title("KMNIST")
+    plt.xlabel("Epoch")
+    plt.ylabel("Test accuracy")
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+    plt.savefig("./part_2_plot.png")
+
+
+def main():
+    models = [Linear(), FeedForward(), CNN()]  # Change during development
+    epochs = 10
+    results = []
+
+    # Can comment the below out during development
+    images, labels = NNModel(Linear(), 0.003).view_batch()
+    print(labels)
+    plt.imshow(images, cmap="Greys")
+    plt.show()
+
+    for model in models:
+        print(f"Training {model.__class__.__name__}...")
+        m = NNModel(model, 0.003)
+
+        accuracies = [0]
+        for e in range(epochs):
+            m.train_epoch()
+            accuracy = m.eval()
+            print(f"Epoch: {e}/{epochs}.. Test Accuracy: {accuracy}")
+            accuracies.append(accuracy)
+        results.append(accuracies)
+
+    plot_result(results, [m.__class__.__name__ for m in models])
+
+
+if __name__ == "__main__":
+    main()
+
